ab1e8c97f351201e1a6dd0f5d9af36eaa0679aab
MichaelNavarret <michael.07navarrete@gmail.com>
2021-12-06 09:30:50 -0300

    modificación carpeta android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, isPlatform} from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,12 +15,29 @@ import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
 import { NgxQRCodeModule } from 'ngx-qrcode2';
 
+/*const getConfig = () => {
+  if (isPlatform('hybrid')){
+    return{
+      backButtonText: 'Previous',
+      tabButtonLayout: 'label-hide'
+    }
+  }
+
+  return{
+    menuIcon: 'ellipsis-vertical'
+  }
+}*/
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, NgxQRCodeModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, NgxQRCodeModule, IonicModule.forRoot({
+    rippleEffect:false,
+    mode:'md'
+  }),
+  AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
   providers: [ { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, QRScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
